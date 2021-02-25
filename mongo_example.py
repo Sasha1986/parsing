391dc0db091d88071a728ddf7b8a643057c3bcf3
Aleksandr <52604323+Sasha1986@users.noreply.github.com>
2021-02-25 16:12:56 +0300

    Delete mongo_example.py

--- mongo_example.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-
-db_client = pymongo.MongoClient("mongodb://localhost:27017")
-db = db_client["gb_data_miner_16_02_2021"]
-collection = db["magnit"]
-
-for item in collection.find():
-    print(item)
